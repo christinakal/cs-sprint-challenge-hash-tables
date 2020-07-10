db73cc1919402ff28542c8a1beff449c53e9ca41
christina kaliora <develop@pirron.one>
2020-07-10 11:46:07 +0200

    ex4 is done - MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,15 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    numbers = dict()
+    result = []
+
+    for number in a:
+        # add the numbers to the dictionary
+        numbers[number] = 1
+        
+        # if the current number's negative version is in the dictionary, add the positive version to result arr
+        # make sure to exclude zero
+        if number != 0 and -number in numbers:
+            result.append(abs(number))
 
     return result
 
